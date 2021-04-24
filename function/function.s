bfddf546e9eeef11ba053da2f867bd6463a69202
Fulvio Denza <fulviodenza823@gmail.com>
2021-04-24 17:37:01 +0200

    finish function.s file

--- function/function.s
+++ function/function.s
@@ -29,3 +29,17 @@ power:
     movl 12(%ebp), %ecx
     movl %ebx, -4(%ebp)
     
+power_loop_start:
+    cmpl $1, %ecx
+    je end_power
+    movl -4(%ebp), %eax
+    imull %ebx, %eax
+    movl %eax, -4(%ebp)
+    decl %ecx
+    jmp power_loop_start
+    
+end_power:
+    movl -4(%ebp), %eax
+    movl %ebp, %esp
+    popl %ebp
+    ret
